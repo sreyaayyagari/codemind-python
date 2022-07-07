9575d74e1394c8820730026e8ed0c0132c8167c7
sreyaayyagari <97898923+sreyaayyagari@users.noreply.github.com>
2022-07-07 11:17:53 +0530

    n=int(input()) t=n if t<0:     n=abs(n) l=len(str(n)) s=0 for i in range(1,l+1):     r=n%10     s=s*10+r     n=n//10 if t<0:     s=-abs(s) print(s)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,13 +1,14 @@
 n=int(input())
 t=n
-if n<0:
+if t<0:
     n=abs(n)
 l=len(str(n))
 s=0
-for i in range (1,l+1):
+for i in range(1,l+1):
     r=n%10
     s=s*10+r
     n=n//10
 if t<0:
     s=-abs(s)
-print(s)
+print(s)    
+    
