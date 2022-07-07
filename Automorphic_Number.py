b15c493de32602fdae6b8455fe1438e8cb4fff5d
sreyaayyagari <97898923+sreyaayyagari@users.noreply.github.com>
2022-07-07 11:07:00 +0530

    n=int(input()) s=n*n l=len(str(n)) a=0 for i in range(1,l+1):     r=s%10     a=a*10+r     s=s//10 a=str(a) a=a[::-1] a=int(a) if n==a:     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,17 +1,15 @@
 n=int(input())
-p=n*n
+s=n*n
 l=len(str(n))
-s=0
+a=0
 for i in range(1,l+1):
-    r=p%10
-    s=s*10+r
-    p=p//10
-s1=0
-for j in range(1,l+1):
-    r1=s%10
-    s1=s1*10+r1
+    r=s%10
+    a=a*10+r
     s=s//10
-if n==s1:
+a=str(a)
+a=a[::-1]
+a=int(a)
+if n==a:
     print("Automorphic Number")
 else:
     print("Not an Automorphic Number")
