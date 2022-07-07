a523db88e6ff46cf2c7272befcea5b821be0e4ae
sreyaayyagari <97898923+sreyaayyagari@users.noreply.github.com>
2022-07-07 10:51:59 +0530

    m,n=map(int,input().split()) max=0 for i in range(1,n):     if m%i==0 and n%i==0:         max=i print(max)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,7 +1,6 @@
-def gcd(a,b):
-    if b==0:
-        return a
-    return gcd(b,a%b)
-a,b=map(int,input().split())
-print(gcd(a,b))
-
+m,n=map(int,input().split())
+max=0
+for i in range(1,n):
+    if m%i==0 and n%i==0:
+        max=i
+print(max)
