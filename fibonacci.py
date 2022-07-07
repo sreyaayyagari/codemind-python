63a03531eea5e94a6e2f64db14b30c61076e69bd
sreyaayyagari <97898923+sreyaayyagari@users.noreply.github.com>
2022-07-07 10:56:16 +0530

    n=int(input()) a=0 b=1 print(a,end=' ') print(b,end=' ') for i in range(n-2):     c=a+b     print(c,end=' ')     a=b     b=c

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,12 @@
-def fib(n):
-    a,b=0,1
-    print(a,b,end=' ')
-    for i in range(2,n):
-        c=a+b
-        print(c,end=' ')
-        a=b
-        b=c
 n=int(input())
-fib(n)
+a=0
+b=1
+print(a,end=' ')
+print(b,end=' ')
+for i in range(n-2):
+    c=a+b
+    print(c,end=' ')
+    a=b
+    b=c
+    
+    
